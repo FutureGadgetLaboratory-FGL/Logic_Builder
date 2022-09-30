a1fe29cde5b1d576fc2d5d9fcea81d408605b0aa
glitchy-coder-exe <99559263+glitchy-coder-exe@users.noreply.github.com>
2022-10-01 03:13:57 +0530

    Solved Basic/#1

---
+++ Basic/C/#1.c
@@ -0,0 +1,17 @@
+/*
+Write a program to input two numbers and print their sum
+*/
+
+// Solved by glitchy-coder-exe
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    int a,b;
+    clrscr();
+    printf("Enter the two numbers: \n");
+    scanf("%d\n%d",&a,&b);
+    printf("The sum of the two numbers is %d",a+b);
+    getch();
+    return 0;
+}
