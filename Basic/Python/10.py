5a413856b064c62c2edbb6aa4b7f1d3e85d9e9cb
Nitin Kumar <40369168+nitinkumar30@users.noreply.github.com>
2022-10-02 02:29:28 +0530

    Add files via upload

---
+++ Basic/Python/10.py
@@ -0,0 +1,9 @@
+# 10. Input 5 digit numbers and calculate sum of its digits.
+
+sum=0
+n1 = int(input('Enter a 5-digit number:'))
+while n1:
+    i = n1%10
+    n1=int(n1/10)
+    sum+=i
+print(sum)
---
+++ Basic/Python/3.py
@@ -0,0 +1,6 @@
+# 3. Input length and breadth and calculate area of rectangle
+
+
+l = int(input('Enter length:'))
+b = int(input('Enter breadth:'))
+print('Area of rectangle with length {} and breadth {} is {}.'.format(l,b,l*b))
