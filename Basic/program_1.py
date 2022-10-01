5b2e907165eb241197e43ca3c7d9278307424d2d
hypernovaradiation <ahmersiddiqui9889@gmail.com>
2022-10-01 16:05:32 +0530

    Solved program_1

---
+++ Basic/program_1.py
@@ -0,0 +1,14 @@
+'''
+Program to input two numbers and print their sum
+'''
+
+# Author - hypernovaradiation
+
+#__main__
+
+num1=int(input("Enter first number : "))
+num2=int(input("Enter second number : "))
+
+result=num1+num2
+
+print("The sum of",num1,"and",num2,"is",result)
