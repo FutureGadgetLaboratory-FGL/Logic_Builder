bf31b83b541fccb3b0a7b6b4568d631f4b314e3c
Krishna7472 <111382029+Krishna7472@users.noreply.github.com>
2022-10-04 01:16:15 +0530

    problem_1.c

--- Basic/problem_1.c
+++ Basic/problem_1.c
@@ -2,16 +2,13 @@
 Write a program to input two numbers and print their sum
 */
 
-// Solved by glitchy-coder-exe
+// Solved by Krishna7472
 
 #include<stdio.h>
-#include<conio.h>
 int main(){
     int a,b;
-    clrscr();
-    printf("Enter the two numbers: \n");
+    printf("Enter two numbers: \n");
     scanf("%d\n%d",&a,&b);
-    printf("The sum of the two numbers is %d",a+b);
-    getch();
+    printf("sum=%d",a+b);
     return 0;
 }
