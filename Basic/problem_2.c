610516fe4fea73e66728bc1c5102cb8643d2a7b8
Krishna7472 <111382029+Krishna7472@users.noreply.github.com>
2022-10-04 01:21:43 +0530

    problem_2.c

--- Basic/problem_2.c
+++ Basic/problem_2.c
@@ -1,18 +1,13 @@
 /*
 Input principal amount, rate and time and calculate S.I.
 */
-
-// Solved by glitchy-coder-exe
-
+// Solved by Krishna7472
 #include<stdio.h>
-#include<conio.h>
 int main(){
     float p,r,t,si;
-    clrscr();
-    printf("Enter the principal amount(in rupees), rate(in percent) and time(in years): \n");
+    printf("Enter the principal amount(in rupees),rate(in percent) and time(in years): \n");
     scanf("%f\n%f\n%f",&p,&r,&t);
-    si=p*r*t/100;
-    printf("The simple interest is Rs. %f",si);
-    getch();
+    si=(p*r*t)/100;
+    printf("The simple interest is Rs. %.3f",si);
     return 0;
 }
