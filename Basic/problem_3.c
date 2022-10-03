54ebf75afac3709e62aba3924cf9f5fe6f68742e
Shashwat Gupta <51000827+theHTMLprogrammer@users.noreply.github.com>
2022-10-04 01:25:28 +0530

    Update problem_3.c

--- Basic/problem_3.c
+++ Basic/problem_3.c
@@ -1,7 +1,23 @@
 /*
 Input length and breadth and calculate area of rectangle
 */
+
+// Solved by glitchy-coder-exe
+
+#include<stdio.h>
+#include<conio.h>
+int main(){
+    float l,b;
+    clrscr();
+    printf("Enter the length and breadth of the rectangle: \n");
+    scanf("%f\n%f",&l,&b);
+    printf("The area of the rectangle is %f",l*b);
+    getch();
+    return 0;
+}
+
 // Solved by Krishna7472
+
 #include<stdio.h>
 int main(){
     float l,b;
