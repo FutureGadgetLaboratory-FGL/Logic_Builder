dcbfbc830d6d675a06f4da36d7bb17fa2b153b7c
Krishna7472 <111382029+Krishna7472@users.noreply.github.com>
2022-10-04 01:24:30 +0530

    problem_3.c

--- Basic/problem_3.c
+++ Basic/problem_3.c
@@ -1,17 +1,12 @@
 /*
 Input length and breadth and calculate area of rectangle
 */
-
-// Solved by glitchy-coder-exe
-
+// Solved by Krishna7472
 #include<stdio.h>
-#include<conio.h>
 int main(){
     float l,b;
-    clrscr();
-    printf("Enter the length and breadth of the rectangle: \n");
+    printf("Length and breadth of rectangle: \n");
     scanf("%f\n%f",&l,&b);
-    printf("The area of the rectangle is %f",l*b);
-    getch();
+    printf("The area of rectangle is %.3f",l*b);
     return 0;
 }
