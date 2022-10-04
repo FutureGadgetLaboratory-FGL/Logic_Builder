51d08eb2a5573b023fef9c5e937bba943b0ad737
A$H <91582711+Ashwani2529@users.noreply.github.com>
2022-10-04 11:55:09 +0530

    problem_9.py

--- Basic/problem_9.c
+++ Basic/problem_9.c
@@ -1,21 +1,11 @@
 /*
 Write a program to Input two numbers and swap them.
 */
-
-// Solved by napalion
-
-#include<stdio.h>
-int main(){
-    int num1,num2;
-    printf("Enter num1: \n");
-    scanf("%d",&num1);
-    printf("Enter num2: \n");
-    scanf("%d",&num2);
-    int aux;
-    aux = num1;
-    num1 = num2;
-    num2 = aux;
-    printf("num1 = %d\n",num1);
-    printf("num2 = %d\n",num2);
-    return 0;
-}
+// Solved by Ashwani2529
+x =int(input("Enter first number: "))
+y =int(input("Enter second number: "))
+temp = x
+x = y
+y = temp
+print('First number after swapping: {}'.format(x))
+print('Second number after swapping: {}'.format(y))
