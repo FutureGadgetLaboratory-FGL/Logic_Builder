3e8d525991385f0b173931042a6e033c64713e1f
A$H <91582711+Ashwani2529@users.noreply.github.com>
2022-10-04 11:51:07 +0530

    problem_15.c

--- Basic/problem_15.py
+++ Basic/problem_15.py
@@ -1,7 +1,15 @@
-# 15. Write a program that print True if the number is odd and False if the number is even.
-# by Nitin Kumar
-
-
-n1 = int(input('Enter 1st number:'))
-print(['True' if n1%2==0 else 'False'][0])
-
+/*
+Write a program that print True if the number is odd and False if the number is even.
+*/
+// solved by Ashwani2529
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Enter number= ");
+    scanf("%d",&n);
+    if (n%2==0)
+    printf("False");
+    else
+    printf("True");
+}
