a5503c35fe16b13e29b50bc54dd14bff899bdcc7
niladri2002 <niladri.sadhu2002@gmail.com>
2022-10-04 16:59:31 +0530

    Solved problem number 20 using language C++ from the Basic section

---
+++ Basic/code/problem_20.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int n, reversed = 0, r;
+
+  cout << "Enter a number: ";
+  cin >> n;
+
+  while(n != 0) 
+  {
+    r = n % 10;
+    reversed = reversed * 10 +r;
+    n /= 10;
+  }
+
+  cout << "Reversed Number = " << reversed;
+
+  return 0;
+}
