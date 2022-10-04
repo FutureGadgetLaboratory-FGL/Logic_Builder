696e069c6007a80c4e68532c2b7db55bf46dfc90
A$H <91582711+Ashwani2529@users.noreply.github.com>
2022-10-04 11:41:33 +0530

    updated-problem_18.c

--- Basic/problem_18.c
+++ Basic/problem_18.c
@@ -1,28 +1,19 @@
 /*
 Write a program to input a number and print the sum of its digits.
 */
-
-// solved by hououinkyouma0
-
+// solved by Ashwani2529
 #include<stdio.h>
-
-int sumDigits(int n)
-{
-  int t = n, sum = 0;
-  while (t != 0)
-  {
-    sum += t % 10;
-    t /= 10;
-  }
-  return sum;
-}
-
 int main()
 {
-  int n, sum;
-  printf("Enter the number: ");
-  scanf("%d", &n);
-  sum = sumDigits(n);
-  printf("The sum of its digits = %d", sum);
-  return 0;
+    long int a,sum=0,temp,r;
+    printf("Enter number: ");
+    scanf("%ld",&a);
+    while(a>0)
+    {
+    r=a%10;
+    sum=(sum+r);
+    a=a/10;}
+    return sum;
 }
+
+
