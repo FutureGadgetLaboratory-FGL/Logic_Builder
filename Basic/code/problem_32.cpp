c371276e3615ac339fa1ca06ff508850b2a32fbe
Himanshu P <86120329+HPMASTER8755@users.noreply.github.com>
2022-10-05 01:46:55 +0530

    Added problem 32 solution

---
+++ Basic/code/problem_32.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float a,b, ans;
+    cout<<"Enter the two numbers: ";
+    cin>>a>>b;
+    ans=a*b;
+    cout<<"\nProduct = "<<ans;
+    cout<<endl;
+    return 0;
+}
