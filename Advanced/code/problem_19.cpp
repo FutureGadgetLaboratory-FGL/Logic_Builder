79453a94111495db478ba1991bdafc4aecdb0dc6
meenal0205 <95573151+meenal0205@users.noreply.github.com>
2022-10-05 14:37:28 +0530

    Update problem_19.cpp

--- Advanced/code/problem_19.cpp
+++ Advanced/code/problem_19.cpp
@@ -1,3 +1,17 @@
+// Find the largest rectangular area possible in a given histogram where the largest rectangle can be made of a number of contiguous bars whose heights are given in an array. 
+// assume that all bars have the same width and the width is 1 unit.
+// eg   histogram = {6, 2, 5, 4, 5, 1, 6}
+//  _           _
+// | |  _   _  | |
+// | | | |_| | | |
+// | | | | | | | |
+// | |_| | | | | | 
+// | | | | | |_| |
+// | | | | | | | |
+   
+// output 4*3=12
+   
+
 #include <bits/stdc++.h>
 
 using namespace std;
