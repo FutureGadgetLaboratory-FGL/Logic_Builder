46fdab459d350e41a7499fb422ef1e8ca6e727a5
Shashwat Gupta <51000827+theHTMLprogrammer@users.noreply.github.com>
2022-10-05 11:44:01 +0530

    Fixed comment syntax

--- Basic/code/problem_30.py
+++ Basic/code/problem_30.py
@@ -3,12 +3,13 @@ Input radius and Print the surface area of sphere.
 '''
 
 # Solved by Ashwani2529
+
 radius=float(input("Enter Radius: ")) 
 area_sphere=4*(3.14285714286)*(radius*radius)  
 print("Surface area of the sphere= "+str(area_sphere))
 
 
-// solved by PartThaker007
+# solved by PartThaker007
 
 pi = 3.14
 radius = int(input('Enter the radius of a sphere: '))
