d98aca4fb1837c0e2ec368ed4eaca6f6810f4d3e
Jerry Zhu <54565411+Bobliuuu@users.noreply.github.com>
2022-10-06 14:50:45 -0400

    Update problem_29.py

--- Basic/code/problem_29.py
+++ Basic/code/problem_29.py
@@ -1,2 +1,5 @@
+# 29. Find surface area of cube
+# Solution by Bobliuuu
+
 side_length = int(input("Input the side length of the cube: "))
 print("Surface area:", 6 * side_length * side_length) # Print surface area of cube
