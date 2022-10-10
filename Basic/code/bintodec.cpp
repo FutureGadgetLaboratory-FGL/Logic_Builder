7f57915cc58fda1c66f5505626f30055ad2daf0c
Harpreet Singh <92804770+HAPPYS1NGH@users.noreply.github.com>
2022-10-10 21:37:19 +0530

    Create bintodec.cpp
    Converts Binary no. to Decimal no.

---
+++ Basic/code/bintodec.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int bintodec(int n)
+{
+    int d = 0, x = n, c = 0;
+    while (x > 0)
+    {
+        d += x % 10 * pow(2, c++);
+        x /= 10;
+    }
+    return d;
+}
+int32_t main()
+{
+    int a;
+    cin >> a;
+    cout << "Decimal of" << a << " is " << bintodec(a) << endl;
+}
