d4ea7e869dbac77f7cae47252917202692c63bb8
Ashwani Singh <115502503+Ashwani2749@users.noreply.github.com>
2022-10-11 01:37:19 +0530

    problem_46.py

---
+++ Basic/code/problem_46.py
@@ -0,0 +1,8 @@
+# 46. Write a program to concatenate two strings.
+# Solved By Ashwani2749
+
+str1 = str(input("Enter first String: ")) 
+str2 = str(input("Enter Second String: "))
+print("".join([str1, str2]))  
+str3 = " ".join([str1, str2])  
+print(str3)  
