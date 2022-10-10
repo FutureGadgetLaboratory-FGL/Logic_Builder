e6723d54288d7923c5f3b7c3b9eb3c7c4706a8fa
VibhowSingh56 <114613592+VibhowSingh56@users.noreply.github.com>
2022-10-11 00:25:17 +0530

    Problem_26.c

--- Basic/code/problem_26.c
+++ Basic/code/problem_26.c
@@ -4,17 +4,13 @@
 * * * 
 * * * *    */
 
-//Solved by Ashwani2529
+//Solved by VibhowSingh56
 #include <stdio.h>
 int main() {
-   int i, j, rows;
-   printf("Enter the number of rows: ");
-   scanf("%d", &rows);
-   for (i = 1; i <= rows; ++i) {
+   int i, j;
+   for (i = 1; i <= 4; ++i) {
       for (j = 1; j <= i; ++j) {
          printf("* ");
       }
       printf("\n");
-   }
-   return 0;
-}
+   }}
