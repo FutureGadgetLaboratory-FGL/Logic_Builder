eb22fa37041521410b132ca345f4d116bd521c15
Akesh Yadav <85454169+Akesh0909@users.noreply.github.com>
2022-10-12 21:35:31 +0530

    Create Problem_33.cpp
    Level order traversal of a binary tree. Given the root node of the tree and you have to print the value of the level of the node by level.

---
+++ Advanced/CPP/Problem_33.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> levelOrder(TreeNode* root) {
+        vector<int> ans; 
+        
+        if(root == NULL) 
+            return ans; 
+            
+        queue<TreeNode*> q; 
+        q.push(root); 
+        
+        while(!q.empty()) {
+           
+            TreeNode *temp = q.front(); 
+            q.pop(); 
+            
+            if(temp->left != NULL) 
+                q.push(temp->left); 
+            if(temp->right != NULL) 
+                q.push(temp->right); 
+                
+            ans.push_back(temp->val); 
+        }
+        return ans; 
+    }
+};
