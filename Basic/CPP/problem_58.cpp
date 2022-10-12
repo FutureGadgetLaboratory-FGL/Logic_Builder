a27cd1b93826749e2a230ffd7515497c2eabaaf3
ssaurabh3 <115654420+ssaurabh3@users.noreply.github.com>
2022-10-12 22:25:50 +0530

    problem_58.cpp

---
+++ Basic/CPP/problem_58.cpp
@@ -0,0 +1,15 @@
+/*
+Write a program in C++ to print a welcome text in a separate line.
+submitted by:-ssaurabh3
+*/
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Print a welcome text in a separate line :\n";
+	cout << "----------------------------------------------\n";
+    cout << " Welcome to \n" ;
+    cout << " w3resource.com "<<endl ;
+    
+}
