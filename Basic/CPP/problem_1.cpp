071c0f3dd17e591a9de3d3ace3afb47058de0035
Bguptas <115764053+Bguptas@users.noreply.github.com>
2022-10-18 00:21:00 +0530

    Create problem_1.cpp
    CPP program to add 2 integer value and get the sum

---
+++ Basic/CPP/problem_1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    int a, b;
+    cout << "Enter first number  : ";
+    cin >> a;
+    cout << "Enter second number : ";
+    cin >> b;
+     
+    int sum = a + b;
+    cout << "Sum : " << sum << endl;
+}
