6b39989367d8e7488fb52651bfe35e51517050b5
A$H <ashwanix2749@gmail.com>
2023-10-09 23:07:59 +0530

    Update problem_17.py

--- Advanced/Python/problem_17.py
+++ Advanced/Python/problem_17.py
@@ -1,42 +1,34 @@
+from collections import Counter
+
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        if len(t) > len(s): return ""
+        if len(t) > len(s):
+            return ""
+
         target = Counter(t)
-        i = 0
-        count = 0
-		# find the starting point i
-        while i < len(s):
-            if s[i] in target:
-                break
-            i += 1
-        ans = ''
-        j = i
-        while i < len(s) and j < len(s):
+        i = j = 0
+        count = len(t)
+        min_len = float('inf')
+        start = 0
+
+        while j < len(s):
             if s[j] in target:
                 target[s[j]] -= 1
                 if target[s[j]] >= 0:
-                    count += 1
-                #remove redundant charactors
-                while i < len(s):
+                    count -= 1
+
+                while count == 0:
+                    if j - i + 1 < min_len:
+                        min_len = j - i + 1
+                        start = i
+
                     if s[i] in target:
-                        if target[s[i]] < 0:
-                            target[s[i]] += 1
-                        else: break
+                        target[s[i]] += 1
+                        if target[s[i]] > 0:
+                            count += 1
+
                     i += 1
-                # temp solution found
-                if count == len(t):
-                    if ans == '' or len(ans) > j+1-i:
-                        ans = s[i:j+1]
-					# move i forward
-                    target[s[i]] += 1
-                    if target[s[i]] > 0:
-                        count -= 1
-                    i += 1
-                    while i < len(s):
-                        if s[i] in target:
-                            if target[s[i]] < 0:
-                                target[s[i]] += 1
-                            else: break
-                        i += 1
+
             j += 1
-        return ans
+
+        return "" if min_len == float('inf') else s[start:start + min_len]
