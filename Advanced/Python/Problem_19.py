f7d1d9e456dbf9178f5ec69404b756325073d890
A$H <ashwanix2749@gmail.com>
2023-10-09 22:56:21 +0530

    optimized

--- Advanced/Python/Problem_19.py
+++ Advanced/Python/Problem_19.py
@@ -1,40 +1,28 @@
 def max_area_histogram(histogram):
-	stack = list()
+    stack = []
 
-	max_area = 0
-	index = 0
-	while index < len(histogram):
+    max_area = 0
+    index = 0
 
-		if (not stack) or (histogram[stack[-1]] <= histogram[index]):
-			stack.append(index)
-			index += 1
-		else:
-			# pop the top
-			top_of_stack = stack.pop()
+    while index < len(histogram):
+        if not stack or histogram[stack[-1]] <= histogram[index]:
+            stack.append(index)
+            index += 1
+        else:
+            top_of_stack = stack.pop()
+            area = (histogram[top_of_stack] *
+                    ((index - stack[-1] - 1) if stack else index))
+            max_area = max(max_area, area)
 
-			area = (histogram[top_of_stack] *
-					((index - stack[-1] - 1)
-					if stack else index))
+    while stack:
+        top_of_stack = stack.pop()
+        area = (histogram[top_of_stack] *
+                ((index - stack[-1] - 1) if stack else index))
+        max_area = max(max_area, area)
 
-			max_area = max(max_area, area)
-
-	while stack:
-
-		top_of_stack = stack.pop()
-
-		area = (histogram[top_of_stack] *
-				((index - stack[-1] - 1)
-				if stack else index))
-
-		max_area = max(max_area, area)
-
-
-	return max_area
-
+    return max_area
 
 if __name__ == '__main__':
-	hist = [6, 2, 5, 4, 5, 1, 6]
+    hist = [6, 2, 5, 4, 5, 1, 6]
 
-	print("Maximum area is",
-		max_area_histogram(hist))
-
+    print("Maximum area is", max_area_histogram(hist))
